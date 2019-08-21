a4cafcd3e1c063a1a7fd761d3003a5aa695afba1
Gergely Kocsis <koger23@gmail.com>
2019-08-21 22:35:56 +0200

    refactor(*): refactor RelationshipService

--- LevelCounter/Services/IRelationshipService.cs
+++ LevelCounter/Services/IRelationshipService.cs
@@ -6,5 +6,7 @@ namespace LevelCounter.Services
     public interface IRelationshipService
     {
         Task<Relationship> MakeFriendRequest(string firendName, string userId);
+        Task ConfirmRequest(int relationshipId);
+        Task BlockRequest(int relationshipId);
     }
 }
--- LevelCounter/Services/RelationshipService.cs
+++ LevelCounter/Services/RelationshipService.cs
@@ -19,28 +19,79 @@ namespace LevelCounter.Services
         public async Task<Relationship> MakeFriendRequest(string friendName, string userId)
         {
             var user = await accountService.FindUserByIdAsync(userId);
-            var friend = await Task.Run(() =>
+            var friend = context.Users.FirstOrDefault(u => u.UserName == friendName);
+            var existingRelationship = GetRelationshipByNames(friendName, userId);
+            if (CheckRelationshipCanBePending(existingRelationship))
             {
-                return context.Users.FirstOrDefault(u => u.UserName == friendName);
-            });
-            var existingRelationship = context.Relationships
-                .Where(r => r.UserId == userId)
-                .Where(r => r.RelatingUser.UserName == friendName)
-                .SingleOrDefault();
-            if (existingRelationship != null)
-            {
+                existingRelationship.RelationshipState = Relationship.RelationshipStates.PENDING;
+                await UpdateAndSaveAsync(existingRelationship);
                 return existingRelationship;
             }
+            return await CreateNewRelationship(user, friend);
+        }
 
+        public async Task ConfirmRequest(int relationshipId)
+        {
+            var relationship = GetRelationshipById(relationshipId);
+            relationship.RelationshipState = Relationship.RelationshipStates.CONFIRMED;
+            await UpdateAndSaveAsync(relationship);
+        }
+
+        public async Task BlockRequest(int relationshipId)
+        {
+            var relationship = GetRelationshipById(relationshipId);
+            relationship.RelationshipState = Relationship.RelationshipStates.BLOCKED;
+            await UpdateAndSaveAsync(relationship);
+        }
+
+        public async Task DismissRequest(int relationshipId)
+        {
+            var relationship = GetRelationshipById(relationshipId);
+            relationship.RelationshipState = Relationship.RelationshipStates.UNKNOWN;
+            await UpdateAndSaveAsync(relationship);
+        }
+
+        private bool CheckRelationshipCanBePending(Relationship relationship)
+        {
+            if (relationship != null && relationship.RelationshipState != Relationship.RelationshipStates.BLOCKED)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private async Task<Relationship> CreateNewRelationship(ApplicationUser requester, ApplicationUser friend)
+        {
             var relationship = new Relationship
             {
-                User = user,
+                User = requester,
                 RelatingUser = friend,
                 RelationshipState = Relationship.RelationshipStates.PENDING
             };
             await context.Relationships.AddAsync(relationship);
             await context.SaveChangesAsync();
             return relationship;
+        }
+
+        private Relationship GetRelationshipByNames(string friendName, string userId)
+        {
+            return context.Relationships
+                .Where(r => r.UserId == userId)
+                .Where(r => r.RelatingUser.UserName == friendName)
+                .SingleOrDefault();
+        }
+
+        private async Task UpdateAndSaveAsync(Relationship relationship)
+        {
+            context.Update(relationship);
+            await context.SaveChangesAsync();
+        }
+
+        private Relationship GetRelationshipById(int relationshipId)
+        {
+            return context.Relationships
+                .Where(r => r.RelationshipId == relationshipId)
+                .SingleOrDefault();
         }
     }
 }
