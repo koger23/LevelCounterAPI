3888685b7b54497fb00a205327469d83dd3c16dd
Gergely Kocsis <koger23@gmail.com>
2019-10-11 20:48:33 +0200

    feat: add grouping

--- LevelCounter/Hubs/GameHub.cs
+++ LevelCounter/Hubs/GameHub.cs
@@ -1,46 +1,25 @@
-﻿using LevelCounter.Services;
-using Microsoft.AspNetCore.SignalR;
-using Newtonsoft.Json.Linq;
+﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
 namespace LevelCounter.Hubs
 {
     public class GameHub : Hub
     {
-        //public Task SendMessageToGroup(string groupId, string game)
-        //{
-        //    return Clients.Group(groupId).SendAsync("Send", $"{Context.ConnectionId}: {game}");
-        //}
-
-        //public async Task AddToGroup(string groupId)
-        //{
-        //    await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
-
-        //    await Clients.Group(groupId).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupId}.");
-        //}
-
-        //public async Task RemoveFromGroup(string groupId)
-        //{
-        //    await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupId);
-
-        //    await Clients.Group(groupId).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupId}.");
-        //}
-
-        //public Task SendPrivateMessage(string user, string message)
-        //{
-        //    return Clients.User(user).SendAsync("ReceiveMessage", message);
-        //}
-
-        //public void Send(string name, string message)
-        //{
-        //    // Call the broadcastMessage method to update clients.
-        //    Clients.All.SendAsync("broadcastMessage", name, message);
-        //}
-
-        public async Task Send(string gameJson)
+        public async Task Send(string gameJson, string gameId)
         {
-            // Call the broadcastMessage method to update clients.
-            await Clients.All.SendAsync("broadcastMessage", gameJson);
+            await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+            await Clients.Group(gameId)
+                .SendAsync("broadcastMessage", gameJson);
+        }
+
+        public async Task AddToGroup(string gameId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+        }
+
+        public async Task RemoveFromGroup(string gameId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, gameId);
         }
     }
 }
