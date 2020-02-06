87acf8b4f798b0938d3b1b7a7778c890e7c89430
koger23 <koger23@gmail.com>
2020-02-06 21:25:38 +0100

    fix: fixed user sync

--- LevelCounter/Hubs/GameHub.cs
+++ LevelCounter/Hubs/GameHub.cs
@@ -22,23 +22,35 @@ namespace LevelCounter.Hubs
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, gameId);
         }
 
-        public async Task HeartBeat(string gameId, string userId) {
+        public async Task HeartBeat(string gameId, string userId)
+        {
             await Clients.Group(gameId)
                 .SendAsync("isOnline", userId);
         }
 
-        public async Task SyncUser(string gameId, string inGameUser) {
+        public async Task SyncUser(string gameId, string inGameUser, string senderId)
+        {
+            Console.WriteLine("--->" + inGameUser + " sender: " + senderId);
             await Clients.Group(gameId)
                 .SendAsync("user", inGameUser);
         }
 
-        public async Task SyncTime(string gameId, string time) {
+        public async Task SyncTime(string gameId, string startTime)
+        {
+            Console.WriteLine("--->" + " Time: " + startTime);
             await Clients.Group(gameId)
-                .SendAsync("time", time);
+                .SendAsync("time", startTime);
         }
 
-        public async Task SendGameMessage(string gameId, string message) {
+        public async Task SendGameMessage(string gameId, string message)
+        {
             await Clients.Group(gameId).SendAsync("message", message);
+        }
+
+        public async Task SyncRound(string gameId, int round)
+        {
+            Console.WriteLine("--->" + " Round: " + round);
+            await Clients.Group(gameId).SendAsync("round", round);
         }
     }
 }
